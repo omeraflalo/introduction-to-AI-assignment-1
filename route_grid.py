30eeb1409ff86ed63f1b5b3dbf824961c3f7c247
omer aflalo <omer.aflalo@gmail.com>
2022-11-19 23:18:39 +0200

    refactor code

--- route_grid.py
+++ route_grid.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def create_grid(grid_num=1):
     if grid_num == 1:
         with open("grid_1", "rt") as infile:
@@ -7,6 +8,3 @@ def create_grid(grid_num=1):
     if grid_num == 2:
         with open("grid_2", "rt") as infile:
             return np.matrix([list(line.strip()) for line in infile.readlines()]), (0, 0), (138, 138)
-
-
-
